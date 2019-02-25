aa5e21a67e10eadc48902509c167d9ec49409d66
Tiberiu CORBU <tiberiu.corbu@gmail.com>
2019-02-25 09:01:46 +0100

    Remove commeted out code

--- mcu/src/network/Network.cpp
+++ mcu/src/network/Network.cpp
@@ -1,11 +1,11 @@
 #include "Network.h"
 
 void Network::connect() {
-
+    
 }
 
 void Network::disconnect(){
-
+    WiFi.disconnect();
 }
 
 int Network::searchKnownNetworks() {
@@ -13,19 +13,11 @@ int Network::searchKnownNetworks() {
   WiFi.disconnect();
   int n = WiFi.scanNetworks();
   if (n == 0) {
-      Serial.println("no networks found");
+      ESP_LOGI(TAG, "no networks found");
   } else {
       Serial.print(n);
       Serial.println(" networks found");
       for (int i = 0; i < n; ++i) {
-          // Print SSID and RSSI for each network found
-          Serial.print(i + 1);
-          Serial.print(": ");
-          Serial.print(WiFi.SSID(i));
-          Serial.print(" (");
-          Serial.print(WiFi.RSSI(i));
-          Serial.print(")");
-          Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
           if (isNetworkKnown(WiFi.SSID(i))){
 
           }
@@ -37,6 +29,5 @@ int Network::searchKnownNetworks() {
 
 boolean Network::isNetworkKnown(String i) {
   // config->preferences.getString()  i.equals()
-
   return false;
 }
--- mcu/src/network/Network.h
+++ mcu/src/network/Network.h
@@ -1,13 +1,15 @@
 #ifndef _NETWORK_H_
 #define _NETWORK_H_
-  #include "WiFi.h"
-  class Network {
-    public:
-      void connect();
-      void disconnect();
-      int searchKnownNetworks();
-    private:
-      boolean isNetworkKnown(String i);
-  };
+#include "WiFi.h"
+class Network
+{
+public:
+  void connect();
+  void disconnect();
+  int searchKnownNetworks();
+
+private:
+  boolean isNetworkKnown(String i);
+};
 
 #endif
